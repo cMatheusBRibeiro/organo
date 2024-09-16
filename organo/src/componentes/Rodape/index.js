1c3cfd8696349651c8a97928711ef05586985d6b
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2024-09-16 02:00:37 -0300

    #FEAT: Ajustado estilo

--- organo/src/componentes/Rodape/index.js
+++ organo/src/componentes/Rodape/index.js
@@ -2,14 +2,32 @@ import "./Rodape.css";
 
 const Rodape = () => {
     return (
-        <footer className="rodape">
-            <div className="redes-sociais">
-                <img src="/imagens/fb.png" alt="Logo do Facebook"/>
-                <img src="/imagens/tw.png" alt="Logo do Twitter"/>
-                <img src="/imagens/ig.png" alt="Logo do Instagram"/>
-            </div>
-            <img src="/imagens/logo.png" alt="Logo do Organo"/>
-            <h3>Desenvolvido por Alura.</h3>
+        <footer>
+            <section className="redes-sociais">
+                <ul>
+                    <li>
+                        <a href="https://facebook.com" target="_blank">
+                            <img src="/imagens/fb.png" alt="Logo do Facebook"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com" target="_blank">
+                            <img src="/imagens/tw.png" alt="Logo do Twitter"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://instagram.com" target="_blank">
+                            <img src="/imagens/ig.png" alt="Logo do Instagram"/>
+                        </a>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <img src="/imagens/logo.png" alt="Logo do Organo"/>
+            </section>
+            <section>
+                <p>Desenvolvido por Alura.</p>
+            </section>
         </footer>
     )
 }
