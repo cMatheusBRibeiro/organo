e020bf889f588d8a3849504e8d26ea3f6121d1b7
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2024-09-16 00:18:02 -0300

    #FEAT: Criado componente Time

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from "./componentes/Formulario";
+import Time from './componentes/Time';
 
 function App() {
   const [colaboradores, setColaboradores] = useState([]);
@@ -15,6 +16,9 @@ function App() {
       <Formulario
         aoColaboradorCadastrado={(colaborador) => aoColaboradorCadastrado(colaborador)}
       />
+      <Time nome="Programação" />
+      <Time nome="Front-end" />
+      <Time nome="Data Science" />
     </div>
   );
 }
