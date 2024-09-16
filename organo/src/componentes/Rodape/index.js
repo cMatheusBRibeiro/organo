a021b8ee5aaf371a9b632f2ea7f808b4d2871aaf
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2024-09-16 08:10:01 -0300

    #HOT: Adicionado propriedade rel nos links de redes sociais do Rodape

--- organo/src/componentes/Rodape/index.js
+++ organo/src/componentes/Rodape/index.js
@@ -6,17 +6,17 @@ const Rodape = () => {
             <section className="redes-sociais">
                 <ul>
                     <li>
-                        <a href="https://facebook.com" target="_blank">
+                        <a href="https://facebook.com" target="_blank" rel="noreferrer">
                             <img src="/imagens/fb.png" alt="Logo do Facebook"/>
                         </a>
                     </li>
                     <li>
-                        <a href="https://twitter.com" target="_blank">
+                        <a href="https://twitter.com" target="_blank" rel="noreferrer">
                             <img src="/imagens/tw.png" alt="Logo do Twitter"/>
                         </a>
                     </li>
                     <li>
-                        <a href="https://instagram.com" target="_blank">
+                        <a href="https://instagram.com" target="_blank" rel="noreferrer">
                             <img src="/imagens/ig.png" alt="Logo do Instagram"/>
                         </a>
                     </li>
